6d4bda71acd1939e053313187036a4094c60ab1a
PancenOye007 <115642214+PancenOye007@users.noreply.github.com>
2023-03-03 09:59:59 +0700

    Create l468xp60.js

---
+++ l468xp60.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'c6cf35a1c4a4d4d41497f4936920b68d',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/c6cf35a1c4a4d4d41497f4936920b68d/invoke.js"></scr' + 'ipt>');
